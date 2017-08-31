578501a3ab1d8ed67b2cc8d966588065ce8806f7
James Dolan <jwdolan820@gmail.com>
2017-08-31 16:31:41 +0000

    Added version record and module

---
+++ lib/presidents.rb
@@ -0,0 +1,4 @@
+module Presidents #collects, bundles methods for interactive use in Scraper, President, and CLI classes
+end
+
+require_relative '../config/environment' #loads the environment
---
+++ lib/presidents/version.rb
@@ -0,0 +1,3 @@
+module Presidents
+  VERSION = "0.1.0"  #stores the project's version so it may be used at a later date
+end
