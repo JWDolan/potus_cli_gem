05ef9592e106144d2d0b52cd9ba1ca9c93326cae
James Dolan <jwdolan820@gmail.com>
2017-08-31 16:12:18 +0000

    Added initialize method to President class

--- lib/presidents/president.rb
+++ lib/presidents/president.rb
@@ -9,4 +9,10 @@ class Presidents::President #gives Presidents module access to the President cla
       "http://www.ipl.org/div/potus/#{r.css("a").attribute("href").text}" #interpolates link text at the end of the URL
       )
   end
+#instantiates new instance with name, url arguments (default value is nil for both)
+  def initialize(name=nil, url=nil)
+    @name = name  #sets name argument to an instance variable
+    @url = url    #sets url argument to an instance variable
+    @@all << self   #shovel operator adds the instance to the @@all array
+  end
 end
