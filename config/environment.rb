e0fa3ef8641ad7c3744018983f719f4fe06ae9d2
James Dolan <jwdolan820@gmail.com>
2017-08-31 15:13:01 +0000

    Added config/environment.rb

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require 'nokogiri' 	#allows conversion of HTML to a NodeSet of Nokogiri::XML::Elements
+require 'open-uri' 	#allows HTTP requests
+
+#Loads the Scraper, President, and CLI classes
+require_relative '../lib/presidents/scraper'
+require_relative '../lib/presidents/president'
+require_relative '../lib/presidents/cli'
